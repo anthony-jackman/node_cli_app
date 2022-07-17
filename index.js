2fbd434d1e8160864e20030b1e72e7d05aa8e1d9
Anthony Jackman <anthony.jackman@serco-na.com>
2022-07-17 09:38:17 -0500

    Add command details

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import cli from 'commander';
 
 cli
   .description('Access the JSON Placeholder API')
-  .name('jsonp');
+  .name('jsonp')
+  .usage('<command>')
+  .addHelpCommand(false)
+  .helpOption(false);
 
 cli.parse(process.argv);
