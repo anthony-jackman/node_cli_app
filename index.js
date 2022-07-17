d140b2e399d5b8ab66478626aac3a8a7a95ac6e5
Anthony Jackman <anthony.jackman@serco-na.com>
2022-07-17 09:57:50 -0500

    Adds posts command

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
-import cli from 'commander';
+import { Command } from 'commander';
+const cli = new Command();
+
+import posts from './commands/posts.js';
 
 cli
   .description('Access the JSON Placeholder API')
@@ -8,5 +11,12 @@ cli
   .usage('<command>')
   .addHelpCommand(false)
   .helpOption(false);
+
+cli
+  .command("posts")
+  .argument("[postId]", "ID of post you\'d like to retrieve.")
+  .option("-p, --pretty", "Printy print output from the API")
+  .description("Retrieve a list of all posts or one post by passing the post ID (e.g., posts 1).")
+  .action(posts);
 
 cli.parse(process.argv);
